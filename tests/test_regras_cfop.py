4ceeb9d38f98940f9328b0dd1c20903ddfb38f01
Jhonatan Guimaraes Bandeira <jhonatan.bandeira@outlook.com>
2026-08-13 09:51:38 -0300

    Create test_regras_cfop.py

---
+++ tests/test_regras_cfop.py
@@ -0,0 +1,27 @@
+﻿import unittest
+
+from backend.regras_fiscais.cfop.regras_cfop import (
+    cfop_eh_entrada,
+    cfop_eh_saida,
+    cfop_eh_servico,
+)
+
+
+class TestRegrasCfop(unittest.TestCase):
+    def test_cfop_eh_entrada(self):
+        self.assertIs(cfop_eh_entrada("1102"), True)
+        self.assertIs(cfop_eh_entrada("5102"), False)
+
+    def test_cfop_eh_saida(self):
+        self.assertIs(cfop_eh_saida("5102"), True)
+        self.assertIs(cfop_eh_saida("1102"), False)
+
+    def test_cfop_eh_servico(self):
+        self.assertIs(cfop_eh_servico("5102"), False)
+        self.assertIs(cfop_eh_servico("1102"), False)
+        self.assertIs(cfop_eh_servico("1933"), True)
+        self.assertIs(cfop_eh_servico("5933"), True)
+
+
+if __name__ == "__main__":
+    unittest.main()
